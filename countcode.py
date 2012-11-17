dbf9d060e0d231fdbeb8a0db378f1d99e0fad35a
BC_Programming <bc_programming@hotmail.com>
2012-11-16 16:41:11 -0800

    first commit

---
+++ countcode.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import sys
+import fnmatch
+import os
+
+def searchfolder(folder,filemask):
+    filelist=[]
+    for root,subFolders,files in os.walk(folder):
+        for file in files:
+            buildpath=os.path.join(root,file)
+            if os.path.isfile(buildpath):            
+                if fnmatch.fnmatch(buildpath,filemask):
+                    filelist.append(buildpath)
+    return filelist
+    
+
+
+def Countlines(filename):
+    #counts the lines in filename.
+    countit = open(filename)
+    runner=0
+    for line in countit:
+        runner=runner+1
+        
+    return runner
+    
+    
+
+
+searchfor = sys.argv[1]
+searchin = sys.argv[2]
+print("examining files matching " + searchfor + " in directory " + searchin)
+totalcount=0
+for countthese in searchfolder(searchin,searchfor):
+    #print Countlines(countthese)
+    totalcount = totalcount + Countlines(countthese)
+
+print("total line count:" + str(totalcount))
+
+
+
+
